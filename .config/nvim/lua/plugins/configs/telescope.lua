527f8c7913ab6fe1aef9fa136d146bc5c6b9efb3
Kenan Alizadeh <kananalizade@gmail.com>
2025-01-20 14:50:30 +0400

    telescope ignore pattern fix

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -32,7 +32,7 @@ local options = {
       preview_cutoff = 120,
     },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules", ".git", ".DS_Store", "target", "Pods" },
+    file_ignore_patterns = { "node_modules", "^.git", ".DS_Store", "target", "Pods" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "truncate" },
     winblend = 0,
