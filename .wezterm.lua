0e81d464fce04c0919f44d1f838a781ae79b4121
Kenan Alizadeh <kenalizadeh@Kenans-MacBook-Pro.local>
2025-01-29 15:48:00 +0400

    wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
--- config.font_size = 18
+config.font_size = 22
 config.font = wezterm.font 'Victor Mono'
 config.use_fancy_tab_bar = false
 
