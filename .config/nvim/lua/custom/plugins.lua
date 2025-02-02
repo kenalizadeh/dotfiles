f7d3f6b315a00fc353b1692e31b2f9d9c5ce9dcd
Kenan Alizadeh <kenalizadeh@Kenans-MacBook-Pro.local>
2025-02-02 22:25:38 +0400

    lazygit plugin

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -189,6 +189,26 @@ local plugins = {
         ignore_filetypes = { 'NvimTree', 'TelescopePrompt' }
       })
     end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gc", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
   }
 }
 return plugins
