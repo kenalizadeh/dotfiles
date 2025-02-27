30ce175902f50b043fa3423068a045c0050469cc
Kenan Alizadeh <kenalizadeh@Kenans-MacBook-Pro.local>
2025-02-27 21:38:42 +0400

    oil.nvim configs & neo-tree disabled

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -2,10 +2,23 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+    delete_to_trash = true,
+    columns = {
+      'icon',
+      -- 'permissions',
+      -- 'size',
+      -- 'mtime',
+    },
+  },
   -- Optional dependencies
-  -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+  dependencies = { 'echasnovski/mini.icons', opts = {} },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
