4ea684e155732a6bffd49c323602bb6372a257c3
Kenan Alizadeh <kenalizadeh@Kenans-MacBook-Pro.local>
2025-02-27 21:39:29 +0400

    treesitter glsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -950,7 +950,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'swift' },
+      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'glsl', 'swift' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
